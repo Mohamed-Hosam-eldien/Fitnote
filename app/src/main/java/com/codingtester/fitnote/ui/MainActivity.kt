217e8f9ffcc72fa644b3ffda7ecbbfdfafc9d422
mohamedhosam <mohamedhosameldien07@gmail.com>
2022-08-31 14:32:40 +0200

    refactor code and design

--- app/src/main/java/com/codingtester/fitnote/ui/MainActivity.kt
+++ app/src/main/java/com/codingtester/fitnote/ui/MainActivity.kt
@@ -1,8 +1,9 @@
 package com.codingtester.fitnote.ui
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
 import com.codingtester.fitnote.R
@@ -13,28 +14,38 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.bottomNav.setupWithNavController(findNavController(R.id.navHost))
+        navController = findNavController(R.id.navHost)
+
         binding.bottomNav.background = null
         binding.bottomNav.menu.getItem(2).isEnabled = false
 
-        binding.btnStartRun.setOnClickListener {
-            findNavController(R.id.navHost).navigate(R.id.action_homeFragment_to_trackingFragment)
+        binding.bottomNav.setupWithNavController(navController)
+
+        navController.addOnDestinationChangedListener { _, destination, _ ->
+            when (destination.id) {
+                R.id.homeFragment, R.id.settingsFragment, R.id.statisticsFragment ->
+                    binding.cord.visibility = View.VISIBLE
+                else -> binding.cord.visibility = View.GONE
+            }
         }
 
-        findNavController(R.id.navHost)
-            .addOnDestinationChangedListener{_, destination, _ ->
-                when(destination.id) {
-                    R.id.homeFragment, R.id.settingsFragment, R.id.statisticsFragment ->
-                        binding.cord.visibility = View.VISIBLE
-                    else -> binding.cord.visibility = View.GONE
-                }
-            }
+        binding.btnStartRun.setOnClickListener {
+            navController.navigate(R.id.action_homeFragment_to_trackingFragment)
+        }
+
 
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
